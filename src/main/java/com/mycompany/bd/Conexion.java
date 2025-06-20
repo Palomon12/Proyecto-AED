4a48c8a254a722ca4e5eab58c64194f6480425fe
Kevin Palomino <kpalomino295@gmail.com>
2025-06-20 15:28:01 -0500

    Avance 2 AED

--- src/main/java/com/mycompany/bd/Conexion.java
+++ src/main/java/com/mycompany/bd/Conexion.java
@@ -3,33 +3,32 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.bd;
-import java.sql.Connection;
-
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexion {
+    protected Connection conexion;
     private static final String URL = "jdbc:mysql://localhost:3306/gestion_biblioteca";
     private static final String USER = "root";
     private static final String PASS = "Leopoldox1.2.3.4.5";
-    private Connection connection = null;
+    
 
     public Connection conectar() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            connection = DriverManager.getConnection(URL, USER, PASS);
+            conexion = DriverManager.getConnection(URL, USER, PASS);
             System.out.println("Conectada a la bd");
         } catch (ClassNotFoundException | SQLException ex) {
             ex.printStackTrace();
         }
-        return connection;
+        return conexion;
     }
 
     public void cerrar() {
-        if (connection != null) {
-            try { connection.close(); } catch (SQLException e) { e.printStackTrace(); }
+        if (conexion != null) {
+            try { conexion.close(); } catch (SQLException e) { e.printStackTrace(); }
         }
     }
 }
